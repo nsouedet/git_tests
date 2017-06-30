5e4f0528575f9e98d4f84c1f9d6bbd4ce9973592
nsouedet <nicolas.souedet@gmail.com>
2017-06-30 15:23:08 +0200

    test

--- python/git_tests/info.py
+++ python/git_tests/info.py
@@ -3,7 +3,7 @@
 # Current version
 version_major = 1
 version_minor = 1
-version_micro = 0
+version_micro = 1
 version_extra = ""
 
 # The following variables are here for backward compatibility in order to
